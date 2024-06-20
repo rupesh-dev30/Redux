c31b06faed6a6755bc769daf5258918a5e5d6582
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-20 23:32:16 +0530

    older React using Redux like

--- src/store.js
+++ src/store.js
@@ -53,12 +53,26 @@ const store = createStore(reducer);
 // store.dispatch({type: "account/payLoan"});
 // console.log(store.getState());
 
+const ACCOUNT_DEPOSIT = "account/deposit";
+
 function deposit(amount){
-  return { type: "account/deposit", payload: amount }
+  return { type: ACCOUNT_DEPOSIT, payload: amount }
 }
-function withdraw(){}
-function requestLoan(){}
-function payLoan(){}
+function withdraw(amount){
+  return { type: "account/withdraw", payload: amount }
+}
+function requestLoan(amount, purpose){
+  return { type: "account/requestLoan", payload: {amount: amount, purpose: purpose} }
+}
+function payLoan(){
+  return {type: "account/payLoan"};
+}
 
 store.dispatch(deposit(500))
+console.log(store.getState());
+store.dispatch(withdraw(200))
+console.log(store.getState());
+store.dispatch(requestLoan(1000, "Buy a toy car"))
+console.log(store.getState());
+store.dispatch(payLoan());
 console.log(store.getState());
