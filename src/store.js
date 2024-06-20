a29ad922ff7adff9e78903ff9ab24c115cf56453
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-20 23:03:57 +0530

    Redux Basic

---
+++ src/store.js
@@ -0,0 +1,30 @@
+import { type } from "@testing-library/user-event/dist/type";
+import { createStore } from "redux";
+
+const initialState = {
+  balance: 0,
+  load: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action){
+  switch(action.type){
+    case "account/deposit":
+      return {...state, balance: state.balance + action.payload};
+    case "account/withdraw":
+      return {...state, balance: state.balance - action.package};
+    case "account/requestLoan":
+      if(state.loan > 0) return state;
+      //LATER
+      return {...state, loan: action.payload};
+    case "account/payLoan":
+      return {...state, loan: 0, loanPurpose: "", balance: state.balance - state.loan};
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+store.dispatch({type: 'account/deposit', payload: 500});
+
+console.log("Hey Redux");
